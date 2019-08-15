a85891e238c980ad2cc0bd0712b93392def0af1c
smruthi19 <49352031+smruthi19@users.noreply.github.com>
2019-08-15 12:46:03 -0400

    Add files via upload

--- final.js
+++ final.js
@@ -50,22 +50,28 @@ function load(){
     console.log(urlink);
     var image=document.createElement('img');
     var para=document.createElement("P");
+    var para2=document.createElement("Pa");
+    var para3=document.createElement("Par");
     var link=document.createElement('a');
     image.setAttribute("src",img);
     image.setAttribute("width","300");
     image.setAttribute("height","300");
     image.setAttribute("alt","image");
     image.setAttribute("align","center");
-    image.setAttribute("float","initial")
+    image.setAttribute("float","initial");
+    image.setAttribute("style","padding:20px;");
+    //document.getElementById("img").style.padding="100px 10px 20px 30px";
 
 
 
     link.setAttribute('href',urlink);
-    link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+    link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
     // var para2=document.createElement("Pa");
     link.innerHTML=title;
     para.innerHTML="";
+    // para2.innerHTML="";
+    // para3.innerHTML="";
 
     // para2.innerHTML="";
 
@@ -110,11 +116,12 @@ function load(){
        image.setAttribute("alt","image");
        image.setAttribute("align","center");
        image.setAttribute("float","initial")
+       image.setAttribute("style","padding:20px;");
 
 
 
        link.setAttribute('href',urlink);
-       link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+       link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
 
@@ -168,11 +175,11 @@ function load(){
          image.setAttribute("alt","image");
          image.setAttribute("align","center");
          image.setAttribute("float","initial")
-
+         image.setAttribute("style","padding:20px;");
 
 
          link.setAttribute('href',urlink);
-         link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+         link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
 
@@ -221,11 +228,12 @@ function load(){
            image.setAttribute("alt","image");
            image.setAttribute("align","center");
            image.setAttribute("float","initial")
+           image.setAttribute("style","padding:20px;");
 
 
 
            link.setAttribute('href',urlink);
-           link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+           link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;");
 
 
            // var para2=document.createElement("Pa");
@@ -244,60 +252,7 @@ function load(){
          }
         }
         function loadmx(){
-           var url="https://newsapi.org/v2/top-headlines?country=mx&apiKey=32da0e5f7f6f418ba0dc4ad64e712c4c";
-           var title
-           // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-           var Request=new XMLHttpRequest();
-           Request.open('GET',url, false);
-           Request.send();
-
-
-           if(Request.readyState!=4 ||  Request.status!= 200 || Request.responseText===""){
-           window.console.error("Request had an error");
-           return;
-           }
-           var information=JSON.parse(Request.responseText);
-           for (var i=0;i<9;i++){
-             var title= information.articles[i].title;
-             var urlink=information.articles[i].url;
-             var img=information.articles[i].urlToImage;
-
-             console.log(title);
-             console.log(urlink);
-             var image=document.createElement('img');
-             var para=document.createElement("P");
-             var link=document.createElement('a');
-             image.setAttribute("src",img);
-             image.setAttribute("width","300");
-             image.setAttribute("height","300");
-             image.setAttribute("alt","image");
-             image.setAttribute("align","center");
-             image.setAttribute("float","initial")
-
-
-
-             link.setAttribute('href',urlink);
-             link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
-
-
-
-             // var para2=document.createElement("Pa");
-             link.innerHTML=title;
-             para.innerHTML="";
-
-             // para2.innerHTML="";
-
-             document.body.appendChild(para);
-             document.body.appendChild(image);
-             // document.body.appendChild(para);
-             document.body.appendChild(link);
-             //document.getElementById('a').style.color="red";
-             // document.body.appendChild(para2);
-
-           }
-          }
-          function loadsa(){
-             var url="https://newsapi.org/v2/top-headlines?country=za&apiKey=32da0e5f7f6f418ba0dc4ad64e712c4c";
+             var url="https://newsapi.org/v2/top-headlines?country=mx&apiKey=32da0e5f7f6f418ba0dc4ad64e712c4c";
              var title
              // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
              var Request=new XMLHttpRequest();
@@ -325,12 +280,13 @@ function load(){
                image.setAttribute("height","300");
                image.setAttribute("alt","image");
                image.setAttribute("align","center");
-               image.setAttribute("float","initial")
+               image.setAttribute("float","initial");
+               image.setAttribute("style","padding:20px");
 
 
 
                link.setAttribute('href',urlink);
-               link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+               link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;");
 
 
                // var para2=document.createElement("Pa");
@@ -348,6 +304,59 @@ function load(){
 
              }
             }
+          // function loadsa(){
+          //    var url="https://newsapi.org/v2/top-headlines?country=za&apiKey=32da0e5f7f6f418ba0dc4ad64e712c4c";
+          //    var title
+          //    // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+          //    var Request=new XMLHttpRequest();
+          //    Request.open('GET',url, false);
+          //    Request.send();
+          //
+          //
+          //    if(Request.readyState!=4 ||  Request.status!= 200 || Request.responseText===""){
+          //    window.console.error("Request had an error");
+          //    return;
+          //    }
+          //    var information=JSON.parse(Request.responseText);
+          //    for (var i=0;i<9;i++){
+          //      var title= information.articles[i].title;
+          //      var urlink=information.articles[i].url;
+          //      var img=information.articles[i].urlToImage;
+          //
+          //      console.log(title);
+          //      console.log(urlink);
+          //      var image=document.createElement('img');
+          //      var para=document.createElement("P");
+          //      var link=document.createElement('a');
+          //      image.setAttribute("src",img);
+          //      image.setAttribute("width","300");
+          //      image.setAttribute("height","300");
+          //      image.setAttribute("alt","image");
+          //      image.setAttribute("align","center");
+          //      image.setAttribute("float","initial");
+          //      image.setAttribute("style","padding:20px;");
+          //
+          //
+          //
+          //      link.setAttribute('href',urlink);
+          //      link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
+          //
+          //
+          //      // var para2=document.createElement("Pa");
+          //      link.innerHTML=title;
+          //      para.innerHTML="";
+          //
+          //      // para2.innerHTML="";
+          //
+          //      document.body.appendChild(para);
+          //      document.body.appendChild(image);
+          //      // document.body.appendChild(para);
+          //      document.body.appendChild(link);
+          //      //document.getElementById('a').style.color="red";
+          //      // document.body.appendChild(para2);
+          //
+          //    }
+          //   }
             function loadca(){
                var url="https://newsapi.org/v2/top-headlines?country=ca&apiKey=32da0e5f7f6f418ba0dc4ad64e712c4c";
                var title
@@ -377,12 +386,13 @@ function load(){
                  image.setAttribute("height","300");
                  image.setAttribute("alt","image");
                  image.setAttribute("align","center");
-                 image.setAttribute("float","initial")
+                 image.setAttribute("float","initial");
+                 image.setAttribute("style","padding:20px;");
 
 
 
                  link.setAttribute('href',urlink);
-                 link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+                 link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
 
@@ -431,11 +441,12 @@ function load(){
                    image.setAttribute("alt","image");
                    image.setAttribute("align","center");
                    image.setAttribute("float","initial")
+                   image.setAttribute("style","padding:20px;");
 
 
 
                    link.setAttribute('href',urlink);
-                   link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+                   link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
                    // var para2=document.createElement("Pa");
@@ -483,6 +494,7 @@ function load(){
                      image.setAttribute("alt","image");
                      image.setAttribute("align","center");
                      image.setAttribute("float","initial")
+                     image.setAttribute("style","padding:20px;");
 
                      function myfunction(){
                        var x= document.getElementById('a');
@@ -495,7 +507,7 @@ function load(){
                      }
 
                      link.setAttribute('href',urlink);
-                     link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+                     link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
                      // var para2=document.createElement("Pa");
@@ -543,11 +555,12 @@ function load(){
                        image.setAttribute("alt","image");
                        image.setAttribute("align","center");
                        image.setAttribute("float","initial")
+                       image.setAttribute("style","padding:20px;");
 
 
 
                        link.setAttribute('href',urlink);
-                       link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+                       link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
 
@@ -597,11 +610,12 @@ function load(){
                        image.setAttribute("alt","image");
                        image.setAttribute("align","center");
                        image.setAttribute("float","initial")
+                       image.setAttribute("style","padding:20px;");
 
 
 
                        link.setAttribute('href',urlink);
-                       link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; font-weight:bold; text-align:center;", );
+                       link.setAttribute("style", "background-color:none;font-family: Garmond; color:black; font-size:30px; text-align:center;", );
 
 
                        // var para2=document.createElement("Pa");
